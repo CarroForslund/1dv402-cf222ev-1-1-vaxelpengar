5599a61a9e88886d405bc2d9a82a0e83cefbe55c
CarroForslund <info@carolineforslund.se>
2014-09-09 13:51:37 +0200

    uppdaterade filer

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApplication1
 {
     class Program
     {
+        // Publika variabler
         public static int recievedMoney; // För att kunna använda variabeln i try-catch-satsen
         public static uint fivehundredBills;
         public static uint onehundredBills;
@@ -22,14 +23,14 @@ namespace ConsoleApplication1
         {
             Console.Title = ("Växelpengar - nivå A");
 
-            // Deklarera variabler.
+            // Deklarera lokala variabler
             uint total;                 // Att betala
-            double subtotal;
+            double subtotal;            // Totalt
             double roundingOffAmount;   // Öresavrundning
-            string input;
-            int moneyBack;    
+            string input;               // Inmatad information
+            int moneyBack;              // Växel
 
-            // Läs in värde från tangentbord.
+            // Läs in värde (köpbelopp) från tangentbord
             while (true)
             {
                 Console.Write("Ange totalsumma:");
@@ -38,25 +39,37 @@ namespace ConsoleApplication1
                 {
                     subtotal = double.Parse(input);
 
+                    // Om köpbeloppet är minst en krona - gå vidare
                     if (subtotal >= 1)
                     {
                         break;
                     }
+                    // Annars skriv ut meddelande och avsluta programmet
                     else
                     {
                         Console.BackgroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Totalsumman är för liten. Köpet kunde inte genomföras. Försök igen.");
+                        Console.WriteLine("Totalsumman är för liten. Köpet kunde inte genomföras.");
                         Console.ResetColor();
+                        return;
                     }
                 }
+                // Vid felaktigt inmatat köpbelopp - skriv ut meddelande
                 catch (FormatException)
                 {
                     Console.BackgroundColor = ConsoleColor.Red;
                     Console.WriteLine("FEL! Inmatat belopp är felaktigt. Försök igen");
                     Console.ResetColor();
                 }
+                // Vid okänt fel - skriv ut meddelande
+                catch
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Okänt fel. Försök igen.");
+                    Console.ResetColor();
+                }
             }
 
+            // Läs in värde (mottaget belopp) från tangentbord
             while (true)
             {
                 Console.Write("Ange erhållet belopp:");
@@ -65,27 +78,38 @@ namespace ConsoleApplication1
                 {
                     recievedMoney = int.Parse(input);
 
+                    // Gå vidare om det mottagna beloppet är större än  köpsumman
                     if (recievedMoney >= subtotal)
                     {
                         break;
                     }
+                    // Annars skriv ut meddelande och avsluta programmet
                     else
                     {
                         Console.BackgroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Erhållet belopp är för litet. Köpet kunde inte genomföras. Försök igen.");
+                        Console.WriteLine("Erhållet belopp är för litet. Köpet kunde inte genomföras.");
                         Console.ResetColor();
+                        return;
                     }
 
                 }
+                // Vid felaktigt inmatat belopp skriv ut meddelande
                 catch (FormatException)
                 {
                     Console.BackgroundColor = ConsoleColor.Red;
                     Console.WriteLine("FEL! Erhållet belopp felaktigt. Försök igen.");
                     Console.ResetColor();
                 }
+                // Vid okänt fel skriv ut meddelande
+                catch
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Okänt fel. Försök igen.");
+                    Console.ResetColor();
+                }
             }
 
-            // Räkna ut växelpengar.
+            // Räkna ut växelpengar
             total = (uint)Math.Round(subtotal);
             roundingOffAmount = total - subtotal;
             moneyBack = recievedMoney - (int)total;
@@ -105,46 +129,46 @@ namespace ConsoleApplication1
             remainingSum = remainingSum % 5;
             oneCoins = remainingSum;
 
-            // Skriv ut kvitto med beräknad växel.
+            // Skriv ut kvitto med beräknad växel
             Console.WriteLine("");
             Console.WriteLine("KVITTO");
             Console.WriteLine("------------------------------------------");
-            Console.WriteLine("Totalt         :      {0,8:c}", subtotal);
-            Console.WriteLine("Öresavrundning :      {0,8:c}", roundingOffAmount);
-            Console.WriteLine("Att betala     :      {0,8:c0}", total);
-            Console.WriteLine("Kontant        :      {0,8:c0}", recievedMoney);
-            Console.WriteLine("Tillbaka       :      {0,8:c0}", moneyBack);
+            Console.WriteLine("Totalt             :      {0, 10:c}", subtotal);
+            Console.WriteLine("Öresavrundning     :      {0, 10:c}", roundingOffAmount);
+            Console.WriteLine("Att betala         :      {0, 10:c0}", total);
+            Console.WriteLine("Kontant            :      {0, 10:c0}", recievedMoney);
+            Console.WriteLine("Tillbaka           :      {0, 10:c0}", moneyBack);
             Console.WriteLine("-----------------------------------------");
             if (recievedMoney > subtotal)
             {
                 Console.WriteLine("Att ge tillbaka:");
                 if (fivehundredBills > 0)
                 {
-                    Console.WriteLine("500-lappar \t: {0}", fivehundredBills);
+                    Console.WriteLine("500-lappar         : {0}", fivehundredBills);
                 }
                 if (onehundredBills > 0)
                 {
-                    Console.WriteLine("100-lappar \t: {0}", onehundredBills);
+                    Console.WriteLine("100-lappar         : {0}", onehundredBills);
                 }
                 if (fiftyBills > 0)
                 {
-                    Console.WriteLine("50-lappar \t: {0}", fiftyBills);
+                    Console.WriteLine("50-lappar          : {0}", fiftyBills);
                 }
                 if (twentyBills > 0)
                 {
-                    Console.WriteLine("20-lappar \t : \t {0}", twentyBills);
+                    Console.WriteLine("20-lappar          : {0}", twentyBills);
                 }
                 if (tenCoins > 0)
                 {
-                    Console.WriteLine("10-kronor \t : \t {0}", tenCoins);
+                    Console.WriteLine("10-kronor          : {0}", tenCoins);
                 }
                 if (fiveCoins > 0)
                 {
-                    Console.WriteLine("5-kronor \t : \t {0}", fiveCoins);
+                    Console.WriteLine("5-kronor           : {0}", fiveCoins);
                 }
                 if (oneCoins > 0)
                 {
-                    Console.WriteLine("1-kronor \t : \t {0}", oneCoins);
+                    Console.WriteLine("1-kronor           : {0}", oneCoins);
                 }
             }
         }
